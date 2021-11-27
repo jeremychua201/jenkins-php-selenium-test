d6827ce655c1310d8f3066edc41e72d8e83ac7e0
jeremychua201 <56007710+jeremychua201@users.noreply.github.com>
2021-11-28 02:55:09 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,5 @@
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\Users\\jerem\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\jerem\\Documents\\GitHub\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
