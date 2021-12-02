15cd64bcb64f39374a8e7f757e8cc71a20ae3e1e
jeremychua201 <56007710+jeremychua201@users.noreply.github.com>
2021-12-02 15:20:02 +0800

    change

---
+++ src/newpage.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Search</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<?php
+		echo $_GET['text'] ;
+	?>
+	<a href='index.php'>
+		back to home
+	</a>
+</body>
+</html>
---
+++ src/search.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Search</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<?php
+header('Location: '.$index.php);
+	if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		$status = true
+		if ($status {
+			header('Location: '.$newpage?text=$_POST['fname']);
+		}
+	} else {
+		header('Location: '.$index.php);
+	}
+?>
+	Attacked by Prans
+	<a href='index.php'>
+		<button>OK
+		</button>
+	</a>
+</body>
+</html>
